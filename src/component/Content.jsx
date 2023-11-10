e2cdf1afb1b315254e4580b19bd0bf32a0f212ab
tushargola1 <87809407+tushargola1@users.noreply.github.com>
2023-11-10 17:28:08 +0530

    Update Content.jsx

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -22,9 +22,9 @@ export default function Content(props) {
       <div className="advice-box">
         <h4>ADVICE {id}</h4>
         <p>"{advice}"</p>
-        <img src="src\images\pattern-divider-desktop.svg" alt="pattern" />
+        <img src=".\src\images\pattern-divider-desktop.svg" alt="pattern" />
         <div className="dice">
-          <img src="src\images\icon-dice.svg" alt="dice" onClick={newAdvice} />
+          <img src=".\src\images\icon-dice.svg" alt="dice" onClick={newAdvice} />
         </div>
       </div>
     </>
